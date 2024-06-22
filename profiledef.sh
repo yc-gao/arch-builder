5820136a5bc4961922b977efe5d2cc436febdefe
yinchaogao <yinchaogao@deeproute.ai>
2024-06-22 22:02:45 +0800

    refactor

--- profiledef.sh
+++ profiledef.sh
@@ -2,19 +2,19 @@
 # shellcheck disable=SC2034
 
 iso_name="archiso"
-iso_label="arch_$(date +%Y%m)"
-iso_publisher="xundaoxd <https://github.com/xundaoxd>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux Livecd"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
+buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+           'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd')
-
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
+bootstrap_tarball_compression=(zstd -c -T0 --long -19)
 file_permissions=(
-    ["/home/xundaoxd/"]="1000:1000:0700"
+  ["/home/xundaoxd/"]="1000:1000:0700"
 )
