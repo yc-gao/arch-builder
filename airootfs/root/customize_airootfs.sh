f5ca91855baecd3429f3d3ee490303b8a3bc2cd7
xundaoxd <xundaoxd@outlook.com>
2024-06-23 16:20:44 +0800

    update pacman

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+pacman-key --init
+pacman-key --populate
+
 systemctl enable NetworkManager
 systemctl enable sshd
 systemctl enable sddm
