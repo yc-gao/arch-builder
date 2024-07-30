d6187a74353256a01a6a5d934d731da7782ab6f6
yinchaogao <yinchaogao@deeproute.ai>
2024-07-30 15:17:08 +0800

    update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -11,8 +11,8 @@ systemctl enable sddm
 
 echo "root:demo1234" | chpasswd
 
-useradd -m -s /bin/zsh xundaoxd
-usermod -aG wheel xundaoxd
+useradd -m -s /bin/zsh demo
+usermod -aG wheel demo
 sed -E -i '/^#\s*%wheel.*NOPASSWD/{s/^#\s*//}' /etc/sudoers
-echo "xundaoxd:demo1234" | chpasswd
+echo "demo:demo1234" | chpasswd
 
