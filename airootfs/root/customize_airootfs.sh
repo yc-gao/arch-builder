bb8011f8bf1acbaca155d018087417c22f6647b5
ycgao <yc_x@outlook.com>
2024-08-11 22:04:58 +0800

    update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -5,5 +5,9 @@ systemctl enable sshd
 
 useradd -m -s /bin/zsh demo
 echo "demo:demo1234" | chpasswd
+
+usermod -aG wheel "demo"
+sed -E -i 's/#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/\1/' /etc/sudoers
+
 chown -Rh demo:demo /home/demo
 
