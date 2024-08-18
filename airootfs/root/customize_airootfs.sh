1155a6088ddd8ba615d20ec201cc989e1af519c4
ycgao <yc_x@outlook.com>
2024-08-18 15:36:03 +0800

    update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-systemctl enable NetworkManager
-systemctl enable sshd
-
 useradd -m -s /bin/zsh demo
 echo "demo:demo1234" | chpasswd
+chown -Rh demo:demo /home/demo
 
 usermod -aG wheel "demo"
 sed -E -i 's/#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/\1/' /etc/sudoers
 
-chown -Rh demo:demo /home/demo
+systemctl enable NetworkManager
+systemctl enable sshd
 
