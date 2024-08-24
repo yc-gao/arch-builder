b7133dab634137536421ac43ec4b78596514d493
ycgao <yc_x@outlook.com>
2024-08-24 17:59:43 +0800

    fix pacman

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -7,6 +7,10 @@ chown -Rh demo:demo /home/demo
 usermod -aG wheel "demo"
 sed -E -i 's/#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/\1/' /etc/sudoers
 
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+pacman-key --init
+pacman-key --populate
+
 systemctl enable NetworkManager
 systemctl enable sshd
 
