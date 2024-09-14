df83d27a2cc407818fbdb05f8d234df98a01be8d
ycgao <yc_x@outlook.com>
2024-09-14 21:28:31 +0800

    fix permission

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+opt_user="demo"
+opt_password="demo1234"
+
 useradd -m -s /bin/zsh demo
-echo "demo:demo1234" | chpasswd
-chown -Rh demo:demo /home/demo
+echo "${opt_user}:${opt_password}" | chpasswd
 
 usermod -aG wheel "demo"
 sed -E -i 's/#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/\1/' /etc/sudoers
+
+chown -Rh "${opt_user}:${opt_user}" "/home/${opt_user}"
+chmod 755 "/home/${opt_user}/.config/bspwm/bspwmrc"
+chmod 755 "/home/${opt_user}/.config/ranger/scope.sh"
 
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
 pacman-key --init
