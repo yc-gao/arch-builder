69ddacfdf213ce191cdaf22000e965732471bbce
yinchaogao <yinchaogao@deeproute.ai>
2024-10-16 14:17:02 +0800

    basic setup

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 
-opt_user="${USERNAME:-demo}"
+opt_user="${USERNAME:-root}"
 opt_password="${PASSWORD:-demo1234}"
 
-useradd -m -s /bin/zsh "${opt_user}"
 echo "${opt_user}:${opt_password}" | chpasswd
-
-usermod -aG wheel "${opt_user}"
-sed -E -i 's/#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/\1/' /etc/sudoers
-
-mv "/home/demo" "/home/${opt_user}"
-chown -Rh "${opt_user}:${opt_user}" "/home/${opt_user}"
-chmod 755 "/home/${opt_user}/.config/bspwm/bspwmrc"
-chmod 755 "/home/${opt_user}/.config/ranger/scope.sh"
 
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
 pacman-key --init
